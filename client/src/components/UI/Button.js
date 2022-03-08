7395ca800bd745d3995e47cb4b015c367edbad60
Temitayo Osunkiyesi <osunkiyesitayo@gmail.com>
2022-03-08 05:44:51 +0100

    Delete client directory

--- client/src/components/UI/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({btnText, onClick}) => {
-  return (
-    <button className='btn w-75 rounded-pill' type='submit' onClick={onClick ? onClick : null}>{btnText}</button>
-  )
-}
-
-export default Button
