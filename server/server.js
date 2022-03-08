f24fdcf50d3b0e40dd7ebc02763c9c9c12930989
dami-laare <osunkiyesitayo@gmail.com>
2022-03-08 05:04:51 +0100

    Adjusted server file

--- server/server.js
+++ server/server.js
@@ -16,8 +16,12 @@ require('dotenv').config({path: 'server/config/config.env'});
 connectDatabase()
 
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT}`)
+const server = app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server started on PORT: ${process.env.PORT || 4000}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello nigga!!!')
 })
 
 // Handling unhandled promise rejections
